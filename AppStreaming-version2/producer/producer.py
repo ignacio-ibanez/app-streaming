3fdffadb5d320bc10874372ac9844d6f575b765c
ignacio <ignacio.iba.mar@gmail.com>
2018-11-19 16:34:36 +0100

    app-streaming

---
+++ AppStreaming-version2/producer/producer.py
@@ -0,0 +1,35 @@
+
+import os
+import urllib.request
+import json
+from kafka import KafkaProducer
+import time
+
+
+#env-variables:
+kafka_server = os.environ["KAFKA_SERVER"]
+kafka_topic = os.environ["KAFKA_TOPIC"]
+data_link = os.environ["DATA_ORIGIN"]
+
+producer = KafkaProducer(bootstrap_servers=kafka_server, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+
+def produce_data(data_link, kafka_topic):
+	data = read_data(data_link)
+	for message in data:
+		publish(message, kafka_topic)
+
+
+def read_data(data_link):
+	data = urllib.request.urlopen(data_link).read().decode('utf-8')
+	data = json.loads(data)
+	return data
+
+
+def publish(message, kafka_topic):
+	producer.send(kafka_topic, message)
+	
+
+while(True):
+	produce_data(data_link, kafka_topic)
+	time.sleep(60)
